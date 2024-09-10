9910d51dc979daf1dd0b3be0e5989af38f8df491
Slavina Stefanova <sstefanova@wikimedia.org>
2024-09-10 07:27:46 +0200

    add pre-commit

--- app.py
+++ app.py
@@ -1,42 +1,23 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of the Toolforge Python ASGI tutorial
-#
-# Copyright (C) 2023 Slavina Stefanova and contributors
-#
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option)
-# any later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-# more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import os
-import httpx
 
+import httpx
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+
 async def fetch_random_quote():
-    '''Fetch a random quote from the Quotable API, an open-source API (https://github.com/lukePeavey/quotable).
-    '''
+    """Fetch a random quote from the Quotable API, an open-source API (https://github.com/lukePeavey/quotable)."""
     async with httpx.AsyncClient() as client:
-        response = await client.get('https://api.quotable.io/random')
+        response = await client.get("https://api.quotable.io/random")
         if response.status_code == 200:
             return response.json()
         else:
-            raise HTTPException(status_code=response.status_code, detail="Unable to fetch quote")
+            raise HTTPException(
+                status_code=response.status_code, detail="Unable to fetch quote"
+            )
 
 
 @app.get("/healthz")
@@ -57,8 +38,9 @@ async def get_quote():
     return {
         "content": quote_data["content"],
         "author": author,
-        "wikipedia_url": wikipedia_url
+        "wikipedia_url": wikipedia_url,
     }
+
 
 async def get_wikipedia_url(author):
     # Attempt to find a Wikipedia page for the author
@@ -75,10 +57,14 @@ async def get_wikipedia_url(author):
         # If no page is found, return None
         return None
 
-@app.get("/", response_class=HTMLResponse)
+
+@app.get("/")
 async def read_root(request: Request):
-    button_color = os.getenv('BUTTON_COLOR', 'blue')
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "button_color": '#28a745' if button_color == 'green' else '#007bff'
-    })
+    button_color = os.getenv("BUTTON_COLOR", "blue")
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "button_color": "#28a745" if button_color == "green" else "#007bff",
+        },
+    )
